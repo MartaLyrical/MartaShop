a08166d43238738ed210b69896d058267a2b77a2
MartaLyrical <mhsdomingos@gmail.com>
2021-06-08 14:05:11 +0100

    Initial Solution

---
+++ MartaShop/MartaShop.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MartaShop.WebUI.MvcApplication" Language="C#" %>
---
+++ MartaShop/MartaShop.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MartaShop.WebUI.Startup))]
+namespace MartaShop.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
