ba2c6f24cc0d084ff8c6d6f6e8a88437cca3cec7
MartaLyrical <mhsdomingos@gmail.com>
2021-06-29 16:51:51 +0100

    Entity framework created

---
+++ MartaShop/MartaShop.DataAccess.SQL/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace MartaShop.DataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MartaShop.DataAccess.SQL.DataContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(MartaShop.DataAccess.SQL.DataContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
