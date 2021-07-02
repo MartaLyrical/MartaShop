af6570fbb7f06b73bfd916af65d0e5e515a00906
MartaLyrical <mhsdomingos@gmail.com>
2021-07-02 14:10:57 +0100

    Product filter added

---
+++ MartaShop/MartaShop.Core/Models/ProductListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MartaShop.Core.Models;
+
+namespace MartaShop.Core.Models
+{
+    public class ProductListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public IEnumerable<ProductCategory> ProductCategories { get; set; }
+    }
+}
+
