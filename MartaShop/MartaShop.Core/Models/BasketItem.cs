662ffcd1d33341d21069ca8f8c599475e2a681d6
MartaLyrical <mhsdomingos@gmail.com>
2021-07-07 12:03:48 +0100

    Basket Model Added

---
+++ MartaShop/MartaShop.Core/Models/BasketItem.cs
@@ -0,0 +1,9 @@
+﻿namespace MartaShop.Core.Models
+{
+    public class BasketItem : BaseEntity
+    {
+        public string BasketId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
