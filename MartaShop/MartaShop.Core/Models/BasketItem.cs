03a6bd38d1e961a720955763edb872d79ed4865b
MartaLyrical <mhsdomingos@gmail.com>
2021-07-26 12:46:48 +0100

    Basket Service Create and Remove

--- MartaShop/MartaShop.Core/Models/BasketItem.cs
+++ MartaShop/MartaShop.Core/Models/BasketItem.cs
@@ -3,7 +3,7 @@
     public class BasketItem : BaseEntity
     {
         public string BasketId { get; set; }
-        public int ProductId { get; set; }
+        public string ProductId { get; set; }
         public int Quantity { get; set; }
     }
 }
